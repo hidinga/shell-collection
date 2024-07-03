3e55aa8a5a8840f10296b8f6f092c751b42f2bce
hidinga <ping.bao.cn@gmail.com>
2024-07-03 23:01:17 +0800

    Update sed_intersection.bat

--- sed_intersection.bat
+++ sed_intersection.bat
@@ -1,3 +1,5 @@
 @echo off
 
+rem  awk -F[^|] "NR == FNR{ a[$0]; next } !($3 in a) { print $3}" task_list.txt done.txt
+
 awk -F[^|] "{ if(NR == FNR) { a[$0]; next }; if(!($3 in a)) { print $3} }" task_list.txt done.txt
