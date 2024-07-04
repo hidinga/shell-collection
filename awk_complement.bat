c5f10a5a8299ee733d9a41822125fc5e9907a4d3
hidinga <ping.bao.cn@gmail.com>
2024-07-04 11:55:32 +0800

    Update awk_complement.bat

--- awk_complement.bat
+++ awk_complement.bat
@@ -1,5 +1,6 @@
 @echo off
 
-rem  awk -F[^|] "NR == FNR{ a[$1]; next } !($1 in a) { print $1}" task_list.txt done.txt
-
 awk -F[^|] "{ if(NR == FNR) { a[$1]; next }; if(!($1 in a)) { print $1} }" task_list.txt done.txt
+
+rem awk -F[^|] "NR == FNR{ a[$1]; next } !($1 in a)" task_list.txt done.txt
+rem awk -F[^|] "NR == FNR{ a[$1]; next } !($1 in a) { print $1}" task_list.txt done.txt
